a36e294235fdfff40b4c2613e6ee0e4b95d050b4
CinthiaLopez25 <ca.lopez21@info.uas.edu.mx>
2025-04-27 16:20:55 -0700

    tes:mensaje

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -102,6 +102,229 @@
   <a href="#" class="hover:underline">Mis compras</a>
   <i class="fas fa-shopping-cart hover:underline"></i>
 </nav>
-
 </header>
+
+  <div style="text-align: left;">
+  <h2 style="font-size: 48px; font-weight: bold;">Ofertas</h2>
+  </div>
+
+<section class= "p-4 bg-red mb-6" style="background-color:rgb(243, 239, 239);">
+  <!-- Título de sección -->
+  <div class="flex justify-between items-center mb-4">
+    <h2 class="text-4xl font-bold"> Ofertas en celulares y accesorios </h2>
+  </div>
+
+  <!-- Contenedor de productos -->
+  <div class="flex flex-wrap gap-8 justify-center">
+    
+    <!-- Producto 1 -->
+    <div class="w-48">
+    <div class="justify-center; margin: 5px;">
+  <img src="https://http2.mlstatic.com/D_NQ_NP_2X_693146-CBT74076281448_012024-F-2-mica-de-cristal-templado-curvo-3d-para-samsung-s21-ultra-5.webp" alt="Producto 1" class="rounded-lg shadow-md mb-3">
+  <p class="text-sm text-gray-700 mb-1"> 2 Mica De Cristal Templado Curvo 3d Para Samsung S21 Ultra 5 </p>
+  <div class="flex items-baseline gap-2">
+    <span class="text-gray-400 text-xs line-through"> Antes $295.95 </span>
+    <span class="text-black font-bold text-lg"> Ahora $170.90 </span>
+  </div>
+  <p class="text-green-600 text-sm font-semibold mt-1">Envío gratis ⚡ FULL</p>
+
+  <button class="bg-blue-100 text-blue-500 px-2 py-1 mt-2 rounded hover:bg-blue-200 font-bold text-sm flex items-center gap-1">
+      🛒 Añadir al Carrito
+    </button>
+</div>
+</div>
+
+ <!-- Producto 2 -->
+ <div class="w-48" >
+ <div class="justify-center; margin: 5px;">
+      <img src="https://http2.mlstatic.com/D_NQ_NP_2X_928975-CBT50116832035_052022-F-tripode-de-aluminio-con-control-remoto-celular-camera-gopro.webp" alt="Producto 2" class="rounded-lg shadow-md mb-3">
+      <p class="text-sm text-gray-700 mb-1"> Tripode De Aluminio Con Control Remoto Celular. </p>
+      <div class="flex items-baseline gap-2">
+        <span class="text-gray-400 text-xs line-through"> Antes $395.00 </span>
+        <span class="text-black font-bold text-lg"> Ahora $190.00 </span>
+      </div>
+      <p class="text-green-600 text-sm font-semibold mt-1"> Envío gratis ⚡ FULL </p>
+      <button class="bg-blue-100 text-blue-500 px-2 py-1 mt-2 rounded hover:bg-blue-200 font-bold text-sm flex items-center gap-1">
+      🛒 Añadir al Carrito
+    </button>
+    </div>
+    </div>
+
+<!-- Producto 3 -->
+<div class="w-48">
+<div class="justify-center; margin: 5px;">
+      <img src="https://http2.mlstatic.com/D_NQ_NP_2X_753971-CBT81328517352_122024-F-protector-de-cargador-adaptador-para-iphone-tipo-c-dibujo-3d.webp" alt="Producto 3" class="rounded-lg shadow-md mb-3">
+      <p class="text-sm text-gray-700 mb-1"> Protector De Cargador Adaptador Para iPhone Tipo C Dibujo 3d. </p>
+      <div class="flex items-baseline gap-2">
+        <span class="text-gray-400 text-xs line-through"> Antes $199.99 </span>
+        <span class="text-black font-bold text-lg"> Ahora $99.00 </span>
+      </div>
+      <p class="text-green-600 text-sm font-semibold mt-1"> Envío gratis ⚡ FULL </p>
+      <button class="bg-blue-100 text-blue-500 px-2 py-1 mt-2 rounded hover:bg-blue-200 font-bold text-sm flex items-center gap-1">
+      🛒 Añadir al Carrito
+    </button>
+    </div>
+    </div>
+    
+    <!-- Producto 4 -->
+<div class="w-48">
+<div class="justify-center; margin: 5px;">
+      <img src="https://http2.mlstatic.com/D_NQ_NP_2X_760082-MLA81960499439_012025-F.webp" alt="Producto 4" class="rounded-lg shadow-md mb-3">
+      <p class="text-sm text-gray-700 mb-1">Honor Magic7 Lite Jade Cyan 512 GB Con Audífonos de regalo. </p>
+      <div class="flex items-baseline gap-2">
+        <span class="text-gray-400 text-xs line-through">Antes $9,999.00</span>
+        <span class="text-black font-bold text-lg">Ahora $8,590.00</span>
+      </div>
+      <p class="text-green-600 text-sm font-semibold mt-1">Envío gratis ⚡ FULL</p>
+      <button class="bg-blue-100 text-blue-500 px-2 py-1 mt-2 rounded hover:bg-blue-200 font-bold text-sm flex items-center gap-1">
+      🛒 Añadir al Carrito
+    </button>
+    </div>
+    </div>
+        <!-- Producto 5 -->
+        <div class="w-48" >
+        <div class="justify-center; margin: 5px;">
+      <img src="https://http2.mlstatic.com/D_NQ_NP_2X_909012-MLM77337386297_062024-F-audifonos-inalambricos-bluetooth-funda-protectora.webp" alt="Producto 5" class="rounded-lg shadow-md mb-3">
+      <p class="text-sm text-gray-700 mb-1"> Audífonos Inalámbricos Bluetooth + Funda Protectora. </p>
+      <div class="flex items-baseline gap-2">
+        <span class="text-gray-400 text-xs line-through"> Antes $298.00 </span>
+        <span class="text-black font-bold text-lg">Ahora $175.00</span>
+      </div>
+      <p class="text-green-600 text-sm font-semibold mt-1">Envío gratis ⚡ FULL</p>
+      <button class="bg-blue-100 text-blue-500 px-2 py-1 mt-2 rounded hover:bg-blue-200 font-bold text-sm flex items-center gap-1">
+      🛒 Añadir al Carrito
+    </button>
+    </div>
+    </div>
+           <!-- Producto 6 -->
+           <div class="w-48">
+           <div class="justify-center; margin: 5px;">
+      <img src="https://http2.mlstatic.com/D_NQ_NP_2X_983694-MLM83185169963_032025-F-yeemovil-f22-telefono-celular-para-adultos-mayores-4g-telefono-plegable-con-botones-grandes-doble-pantalla-sos-boton-volumen-alto.webp" alt="Producto 6" class="rounded-lg shadow-md mb-3">
+      <p class="text-sm text-gray-700 mb-1"> Yeemovil F22 Teléfono Celular Para Adultos Mayores, 4g Teléfono Plegable Con Botones Grandes, Doble Pantalla, Sos Botón, Volumen Alto. </p>
+      <div class="flex items-baseline gap-2">
+        <span class="text-gray-400 text-xs line-through"> Antes $1,999.00 </span>
+        <span class="text-black font-bold text-lg"> Ahora $899.90 </span>
+      </div>
+      <p class="text-green-600 text-sm font-semibold mt-1">Envío gratis ⚡ FULL</p>
+      <button class="bg-blue-100 text-blue-500 px-2 py-1 mt-2 rounded hover:bg-blue-200 font-bold text-sm flex items-center gap-1">
+      🛒 Añadir al Carrito
+    </button>
+    </div>
+    </div>
+
+  </div>
+</section>
+
+<div class="justify-center; margin: 5px;">
+
+</div>
+
+<!-- Contenedor general -->
+<section class= "p-4 bg-red" style="background-color:rgb(243, 239, 239);">
+  <!-- Título de sección -->
+  <div class="flex justify-between items-center mb-4">
+    <h2 class="text-4xl font-bold">Ofertas relapango para tu baño</h2>
+  </div>
+
+  <!-- Contenedor de productos -->
+  <div class="flex flex-wrap gap-8 justify-center">
+    
+    <!-- Producto 1 -->
+    <div class="w-48">
+    <div class="justify-center; margin: 5px;">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC-E1bSHLLU-wC9icyN3bXHSNK9xtMBfDPhA&s" alt="Producto 1" class="rounded-lg shadow-md mb-3">
+  <p class="text-sm text-gray-700 mb-1">Espejo Luz Led Touch Pared Tocador Baño Vestidor</p>
+  <div class="flex items-baseline gap-2">
+    <span class="text-gray-400 text-xs line-through">Antes $799</span>
+    <span class="text-black font-bold text-lg">Ahora $479</span>
+  </div>
+  <p class="text-sm text-gray-600">en 24 meses de $29</p>
+  <p class="text-green-600 text-sm font-semibold mt-1">Envío gratis ⚡ FULL</p>
+  <button class="bg-blue-100 text-blue-500 px-2 py-1 mt-2 rounded hover:bg-blue-200 font-bold text-sm flex items-center gap-1">
+      🛒 Añadir al Carrito
+    </button>
+</div>
+</div>
+
+ <!-- Producto 2 -->
+ <div class="w-48" >
+ <div class="justify-center; margin: 5px;">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpISC_sYfKSykQQFCOgYrq1BwvJpsrWZ-HIg&s" alt="Producto 2" class="rounded-lg shadow-md mb-3">
+      <p class="text-sm text-gray-700 mb-1">Lavabo de Cerámica Diseño elegante y minimalista.</p>
+      <div class="flex items-baseline gap-2">
+        <span class="text-gray-400 text-xs line-through">Antes $795.50</span>
+        <span class="text-black font-bold text-lg">Ahora $699</span>
+      </div>
+      <p class="text-sm text-gray-600">en 24 meses de $42</p>
+      <p class="text-green-600 text-sm font-semibold mt-1">Envío gratis ⚡ FULL</p>
+      <button class="bg-blue-100 text-blue-500 px-2 py-1 mt-2 rounded hover:bg-blue-200 font-bold text-sm flex items-center gap-1">
+      🛒 Añadir al Carrito
+    </button>
+    </div>
+    </div>
+
+<!-- Producto 3 -->
+<div class="w-48">
+<div class="justify-center; margin: 5px;">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMvTA1w6VtIiKLEZ9ovG6YcBXxc-zOED0dkQ&s" alt="Producto 3" class="rounded-lg shadow-md mb-3">
+      <p class="text-sm text-gray-700 mb-1">Toallero de Acero Inoxidable Resistente y fácil de instalar.</p>
+      <div class="flex items-baseline gap-2">
+        <span class="text-gray-400 text-xs line-through">Antes $295.50</span>
+        <span class="text-black font-bold text-lg">Ahora $199.00</span>
+      </div>
+      <p class="text-green-600 text-sm font-semibold mt-1">Envío gratis ⚡ FULL</p>
+      <button class="bg-blue-100 text-blue-500 px-2 py-1 mt-2 rounded hover:bg-blue-200 font-bold text-sm flex items-center gap-1">
+      🛒 Añadir al Carrito
+    </button>
+    </div>
+    </div>
+    
+    <!-- Producto 4 -->
+<div class="w-48">
+<div class="justify-center; margin: 5px;">
+      <img src="https://cdn.homedepot.com.mx/productos/941874/941874-d.jpg" alt="Producto 4" class="rounded-lg shadow-md mb-3">
+      <p class="text-sm text-gray-700 mb-1">TINA CON HIDROMASAJE. </p>
+      <div class="flex items-baseline gap-2">
+        <span class="text-gray-400 text-xs line-through">Antes $23,950.00</span>
+        <span class="text-black font-bold text-lg">Ahora $15,990.00</span>
+      </div>
+      <p class="text-green-600 text-sm font-semibold mt-1">Envío gratis ⚡ FULL</p>
+      <button class="bg-blue-100 text-blue-500 px-2 py-1 mt-2 rounded hover:bg-blue-200 font-bold text-sm flex items-center gap-1">
+      🛒 Añadir al Carrito
+    </button>
+    </div>
+    </div>
+        <!-- Producto 5 -->
+        <div class="w-48" >
+        <div class="justify-center; margin: 5px;">
+      <img src="https://http2.mlstatic.com/D_NQ_NP_2X_609610-MLU76507372704_052024-F.webp" alt="Producto 5" class="rounded-lg shadow-md mb-3">
+      <p class="text-sm text-gray-700 mb-1">6 Rollos Papel Higiénico Institucional Jumbo Hoja Doble Bio. </p>
+      <div class="flex items-baseline gap-2">
+        <span class="text-gray-400 text-xs line-through">Antes $298.00</span>
+        <span class="text-black font-bold text-lg">Ahora $199.00</span>
+      </div>
+      <p class="text-green-600 text-sm font-semibold mt-1">Envío gratis ⚡ FULL</p>
+      <button class="bg-blue-100 text-blue-500 px-2 py-1 mt-2 rounded hover:bg-blue-200 font-bold text-sm flex items-center gap-1">
+      🛒 Añadir al Carrito
+    </button>
+    </div>
+    </div>
+           <!-- Producto 6 -->
+           <div class="w-48">
+           <div class="justify-center; margin: 5px;">
+      <img src="https://http2.mlstatic.com/D_Q_NP_2X_610199-MLM49360616348_032022-E.webp" alt="Producto 6" class="rounded-lg shadow-md mb-3">
+      <p class="text-sm text-gray-700 mb-1">Papel Higiénico Elite Color Doble Hoja 32 Rollos. </p>
+      <div class="flex items-baseline gap-2">
+        <span class="text-gray-400 text-xs line-through">Antes $175.00</span>
+        <span class="text-black font-bold text-lg">Ahora $115.00</span>
+      </div>
+      <p class="text-green-600 text-sm font-semibold mt-1">Envío gratis ⚡ FULL</p>
+      <button class="bg-blue-100 text-blue-500 px-2 py-1 mt-2 rounded hover:bg-blue-200 font-bold text-sm flex items-center gap-1">
+      🛒 Añadir al Carrito
+    </button>
+    </div>
+    </div>
+
+  </div>
+</section>
 
