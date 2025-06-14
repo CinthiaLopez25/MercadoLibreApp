938c63b24340f263eaba16007a767293c2a02473
Alan Felix <alangiovani123@gmail.com>
2025-06-14 00:09:51 -0700

    Feat: Agregamos el controlador que se encarga de las compras y agregamos la ruta para acceder a la funcionalidad

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware('auth')->group(function() {
   Route::post('/product',[CartController::class, 'addProduct'])->name('cart.addProduct');
+  Route::post('/purchase',[CartController::class, 'store'])->name('purchase.store');
 });
 
 require __DIR__.'/auth.php';
